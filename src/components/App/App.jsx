4251b5e4001f0e57c6468dada07993db04b7c89a
maryna33309 <68459246+marusya2299@users.noreply.github.com>
2025-03-18 20:05:18 +0100

    100

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,26 @@
-
 import ContactList from "../ContactList/ContactList.jsx";
 import SearchBox from "../SearchBox/SearchBox.jsx";
+import ContactForm from "../ContactForm/ContactForm.jsx";
 
-import { useState} from 'react';
+import { useState, useEffect} from 'react';
 
 import data from "../../values.json";
 
+import * as Yup from "yup";
+import { nanoid } from 'nanoid';
+
 export default function App(){
 
-  const [values, setValues]=useState(data);
+  const [values, setValues]=useState(()=>{
+    const savedValues = window.localStorage.getItem("contacts");
+    return savedValues ? JSON.parse(savedValues) : data;
+  });
+
   const [filter, setFilter] = useState('');
 
+  useEffect(() => {
+    window.localStorage.setItem("contacts", JSON.stringify(values));
+  }, [values]);
   
   const deleteValue = (valueId) => {
     setValues((prevValue) => {
@@ -18,14 +28,33 @@ export default function App(){
     });
   };
 
+  const addValue = (nameValue, numberValue) => {
+    const idValue=nanoid();
+    const newValue = { id: idValue, name: nameValue, number: numberValue };
+    setValues((prevValues) => [...prevValues, newValue]);
+  }
+
+
   const visibleValues = values.filter((value) =>
     value.name.toLowerCase().includes(filter.toLowerCase())
   );
+
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .min(3, "Too short!")
+      .max(50, "Too long!")
+      .required("Required!"),
+    number: Yup.string()
+      .min(3, "Too short!")
+      .max(50, "Too long!")
+      .required("Required!"),
+  });
 
   return(
     <div>
       <h1>Phonebook</h1>
         <SearchBox value={filter} onFilter={setFilter} />
+        <ContactForm validationSchema={validationSchema} addValue={addValue} />
         <ContactList values={visibleValues} onDelete={deleteValue}/>
     </div>
 
